0c2c09084eb4427e1ee7ab5ed11cac3af7788543
Michael Coughlin <michael.coughlin@colorado.edu>
2016-11-30 15:36:31 -0700

    added code that implements the same functionality as the SGX packet processor but without SGX

---
+++ NFV_Basic_No_SGX/App.h
@@ -0,0 +1,25 @@
+/*
+ * App.h
+ *
+ *  Created on: Nov 30, 2016
+ *      Author: michael
+ */
+
+#ifndef APP_H_
+#define APP_H_
+
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <arpa/inet.h>
+
+#include "stdlib.h"
+#include <stdio.h>
+#include <iostream>
+#include <string.h>
+#include <assert.h>
+
+#include <unistd.h>
+#include <pwd.h>
+
+
+#endif /* APP_H_ */
